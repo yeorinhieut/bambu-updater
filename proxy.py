78e456f1a3790660fc127ce2de3dd6f599b9ab02
여린히읗 <damhwee@icloud.com>
2024-06-02 14:48:55 +0900

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -6,7 +6,8 @@ import paho.mqtt.client as mqtt
 from datetime import datetime, timedelta
 from threading import Thread, Event
 import queue
-import sys
+import os
+import signal
 
 app = Flask(__name__)
 CORS(app)  # 모든 출처에서의 요청 허용
@@ -23,7 +24,7 @@ def start_shutdown_timer():
         if datetime.now() - last_request_time > TIMEOUT:
             print("Shutting down due to inactivity.")
             shutdown_event.set()
-            sys.exit(0)
+            os.kill(os.getpid(), signal.SIGTERM)
         shutdown_event.wait(60)
 
 @app.route('/ping', methods=['GET'])
@@ -35,7 +36,7 @@ def terminate():
     print("Terminating proxy server.")
     shutdown_event.set()
     response = jsonify({"message": "Terminating server."})
-    response.call_on_close(lambda: sys.exit(0))
+    response.call_on_close(lambda: os.kill(os.getpid(), signal.SIGTERM))
     return response
 
 @app.route('/update', methods=['POST'])
