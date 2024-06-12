d6d11bfe7748d33f45f483c9408f3ad7a0678c10
여린히읗 <yeorinhieut@gmail.com>
2024-06-12 12:46:36 +0900

    automatic model detection

--- proxy.py
+++ proxy.py
@@ -6,6 +6,7 @@ import ssl
 import threading
 import os
 import time
+import requests
 
 app = Flask(__name__)
 CORS(app)
@@ -34,13 +35,20 @@ def terminate():
     response = jsonify({"message": "Terminating server."})
     response.status_code = 200
 
-    # 시스템 명령어로 강제 종료를 스레딩하여 응답을 먼저 보냅니다.
     def shutdown_server():
-        time.sleep(1)  # 클라이언트가 응답을 받을 시간을 줍니다.
+        time.sleep(1)
         os._exit(0)
 
     threading.Thread(target=shutdown_server).start()
     return response
+
+def get_payload(model_name):
+    url = f"https://raw.githubusercontent.com/lunDreame/user-bambulab-firmware/main/assets/{model_name}_AMS.json"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
 
 @app.route('/update', methods=['POST'])
 def update():
@@ -48,7 +56,11 @@ def update():
     printer_ip = data['printerIp']
     device_id = data['sn']
     access_code = data['accessCode']
-    payload = data['payload']  # 이미 JSON으로 파싱된 데이터를 사용
+    model_name = data['printerModel']
+
+    payload = get_payload(model_name)
+    if not payload:
+        return jsonify({"message": "Failed to retrieve payload."}), 400
 
     mqtt_broker = printer_ip
     mqtt_port = 8883
@@ -56,8 +68,7 @@ def update():
     mqtt_password = access_code
     mqtt_topic = f"device/{device_id}/request"
 
-    # MQTT 클라이언트 설정
-    client = mqtt.Client(userdata={'topic': mqtt_topic, 'payload': payload}, protocol=mqtt.MQTTv311)
+    client = mqtt.Client(userdata={'topic': mqtt_topic, 'payload': json.dumps(payload)}, protocol=mqtt.MQTTv311)
     client.username_pw_set(mqtt_username, mqtt_password)
     client.tls_set(cert_reqs=ssl.CERT_NONE)
     client.tls_insecure_set(True)
@@ -66,16 +77,10 @@ def update():
     client.on_connect = on_connect
     client.on_publish = on_publish
 
-    # MQTT 브로커에 연결
     client.connect(mqtt_broker, mqtt_port, 60)
-
-    # 연결 유지 및 메시지 처리 루프 시작
     client.loop_start()
 
     return jsonify({"message": "Update request sent."})
-
-
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=1883)
